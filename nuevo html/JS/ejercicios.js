31eb55e36f506b5ee248d0dba00ceb3560aeadc2
CAJUGATO76 <cajugato76@hotmail.com>
2024-07-25 05:45:56 -0500

    actualización 25 julio 06:00 am

--- nuevo html/JS/ejercicios.js
+++ nuevo html/JS/ejercicios.js
@@ -151,18 +151,85 @@ alert(frutas);
     
 
 let frutas=[];
+let cantidad= frutas.length;
 
 
 function agregarF (){
 
    let frutaN= document.getElementById("fruta");
    let valorF= frutaN.value;
-   frutas.push(valorF);
+   let agregada= document.getElementById("visor");
 
-   let spanResultado = document.getElementById('Todas_las_frutas');
-   spanResultado.textContent = frutas;
-   alert(frutas);
-
-   
+   if (document.getElementById("fruta").value.trim() === "") {
+    alert("Por favor, ingrese un valor en el campo.");
+    // Evitar que se envíe el formulario
+    agregarF.preventDefault();
+    // Mostrar un mensaje o realizar otra acción, por ejemplo:
+    
+    }else if(frutas.includes(valorF)=== true){
+        agregada.textContent= valorF + ". Esta fruta se ha incluido antes en el frutero. ";
+        agregarF.preventDefault();
+    }else{
+    frutas.push(valorF);
+    agregada.textContent=("Agregaste: "+ valorF + " al frutero.");
+    }   
 }
 
+function mostrarF() {
+    cantidad=frutas.length;
+    let spanResultado = document.getElementById("visor");
+    if(frutas.length===0){
+        spanResultado.textContent= "El frutero está vacío!!";
+    }else{
+        spanResultado.textContent = "El frutero contiene: " + frutas + ".";
+    }
+
+    
+    
+}
+function cantidadF() {
+    
+    cantidad=frutas.length;
+    let cantid= document.getElementById("visor");
+    if(frutas.length===0){
+        cantid.textContent="El frutero está vacío!!";
+    }else if(frutas.length===1){
+        cantid.textContent= "Tienes una fruta en el frutero.";
+    }else{
+        cantid.textContent=("tienes " + cantidad + " frutas en el frutero. ");
+    }
+    
+    
+}
+function xUltima(){
+    frutas.pop();
+    let parrafo= document.getElementById("visor");
+    parrafo.textContent= "Has eliminado la última fruta del arreglo.";
+}
+function alInicio() {
+
+    let agregada= document.getElementById("fruta");
+    let frutaI= agregada.value;
+    if(document.getElementById("fruta").value.trim()===""){
+        alert("Por favor ingresa un valor en el campo. ");
+        alInicio.preventDefault();
+    }else{
+        frutas.unshift(frutaI);
+        let parrafo= document.getElementById("visor");
+        parrafo.textContent="Has agregado "+ frutaI + " al inicio del arreglo.";
+    }
+
+}
+function indice() {
+    let parrafo= document.getElementById("fruta");
+    let frutaInd= parrafo.value;
+    let parrafo1=document.getElementById("visor");
+    
+
+    if (frutas.includes(frutaInd)===false){
+        parrafo1.textContent="La fruta no se encuentra en el frutero.";
+    }else{
+        parrafo1.textContent=frutaInd+" se encuentra en la posición: "+ frutas.indexOf(frutaInd) + " del arreglo. ";
+    }
+
+}
